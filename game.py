71d115fc3bd14d9eba24cd4bc9eac709329e050d
Riki Sanjaya <imaderikiwidiastanasanjaya@gmail.com>
2024-05-20 08:21:09 +0800

    starter project of an introduction to game development using python

---
+++ game.py
@@ -0,0 +1,31 @@
+import pygame
+
+pygame.init()
+
+# Game consts
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+DOWN = 0
+UP = 1
+LEFT = 2
+RIGHT = 3
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
+run = True
+while run:
+  # Run game at 60 FPS
+  clock.tick(60)
+  
+  screen.fill((255,255,255))
+  
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+
+  pygame.display.flip()
+    
+
+pygame.quit()
