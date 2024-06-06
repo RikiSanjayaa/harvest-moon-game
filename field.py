1894f486b645b8532284506139f763088556304d
Riki Sanjaya <imaderikiwidiastanasanjaya@gmail.com>
2024-06-06 10:00:42 +0800

    add house and bin sprite

--- field.py
+++ field.py
@@ -21,28 +21,53 @@ class Field:
   TURNIP_ITEM = 2
   DROP_SUCCESS = 9
   
+  HOUSE_TILES = []
+  BIN_TILES = []
+  
   # Field Variables
   ground_tiles = []
   spritesheet = None
   reticle_sprite = None
+  bin_sprite = None
+  house_sprite = None
   screen_rect = pygame.Rect(0, 0, TILE_SIZE, TILE_SIZE)
   tile_rect = pygame.Rect(0, 0, TILE_SIZE, TILE_SIZE)
   reticle_rect = pygame.Rect(383, 417, TILE_SIZE, TILE_SIZE)
+  bin_rect = None
+  house_rect = None
   
   def __init__(self):
+    for house_x in range(0, 8):
+      for house_y in range(0, 6):
+        self.HOUSE_TILES.append((house_x, house_y))
+        
+    for bin_x in range(8, 10):
+      for bin_y in range(4, 6):
+        self.BIN_TILES.append((bin_x, bin_y))
+    
     for x in range(0, self.FIELD_WIDTH):
       field_row = []
       for y in range(0, self.FIELD_HEIGHT):
         rand_spawn = random.randint(0, 20)
-        if rand_spawn == 0:
-          field_row.append(self.ROCK_FRAME)
-        elif rand_spawn == 1:
-          field_row.append(self.WEED_FRAME)
-        else:
+        if (x, y) in self.HOUSE_TILES or (x, y) in self.BIN_TILES:
           field_row.append(random.randint(0, 3))
+        else:
+          if rand_spawn == 0:
+            field_row.append(self.ROCK_FRAME)
+          elif rand_spawn == 1:
+            field_row.append(self.WEED_FRAME)
+          else:
+            field_row.append(random.randint(0, 3))
+          
       self.ground_tiles.append(field_row)
     self.spritesheet = pygame.image.load("field-big.png")
     self.reticle_sprite = pygame.image.load("reticle-big.png")
+    self.bin_sprite = pygame.image.load("bin-big.png").convert_alpha()
+    self.bin_rect = self.bin_sprite.get_rect()
+    self.bin_rect.topleft = (256, 122)
+    self.house_sprite = pygame.image.load("house-big.png").convert_alpha()
+    self.house_rect = self.house_sprite.get_rect()
+    self.house_rect.topleft = (16, 16)
     
   def set_reticle_pos(self, tile_x, tile_y):
     if tile_x < 0:
@@ -109,3 +134,5 @@ class Field:
         self.tile_rect.topleft = (self.TILE_SIZE * cur_col, self.TILE_SIZE * cur_row)
         screen.blit(self.spritesheet, self.screen_rect, self.tile_rect)
         screen.blit(self.reticle_sprite, self.reticle_rect)
+    screen.blit(self.bin_sprite, self.bin_rect)
+    screen.blit(self.house_sprite, self.house_rect)
