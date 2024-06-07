721eba9a5e11cca4ad0b3072091642be570b3192
Riki Sanjaya <imaderikiwidiastanasanjaya@gmail.com>
2024-06-07 20:10:21 +0800

    adding todo list and more assets

--- gameloop.py
+++ gameloop.py
@@ -8,3 +8,10 @@ while running:
   running = game.update()
   
 pygame.quit()
+
+
+# TODO: 1. make more crops available
+# TODO: 2. make a collision between the character and the house and bin sprite
+# TODO: 3. implement the UI showing how much gold/currency the player has with the uibar.png and hmoon font
+# TODO: 4. add a blank page animation to next_day function
+# TODO: 5. properly documented this whole project
